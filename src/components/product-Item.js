6b73ab4d6bf097062d80eaa88ba155759f3d0f46
hafizbima <mas.bima.hafiz@gmail.com>
2023-11-16 12:04:02 +0700

    tes

--- src/components/product-Item.js
+++ src/components/product-Item.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native"; // Correct import
 import { Box, Text, Image, HStack, VStack, Heading } from "native-base";
 
+
 const ProductItem = ({ item }) => {
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={()=> navigation.navigate('ItemDetail', {
+        itemName : item.nama,
+        itemDesc : item.deskripsi,
+        itemPrice : item.harga,
+        itemImage : item.image
+      })}
+      >
       <Box
         height={240}
         width={170}
@@ -16,13 +26,13 @@ const ProductItem = ({ item }) => {
         borderRadius={10}
         shadow={3}
       >
-        <Image source={{ uri: item.image }} height="60%" borderRadius={10} alt="null" />
+        <Image source={{ uri : item.image }} height="60%" borderRadius={10} alt="null" />
         <HStack alignItems={"center"} justifyContent={"space-between"}>
           <VStack space={0.5} mt={2}>
             <Heading size="xs">{item.nama}</Heading>
             <Text fontSize="sm">{item.kategori}</Text>
             <Heading mt={3} size={"sm"}>
-              IDR : {item.harga}
+              IDR: {item.harga}
             </Heading>
           </VStack>
         </HStack>
--- src/screens/AppStack.js
+++ src/screens/AppStack.js
@@ -4,6 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import Register from './register'
 import Tabs from './(tabs)/_layout';
 import Discount from './discount';
+import Address from './(detail)/Address';
+import Checkout from './(detail)/Checkout';
+import ItemDetail from './(detail)/ItemDetail';
 
 const Stack = createNativeStackNavigator();
 const noHead = { headerShown: false };
@@ -15,6 +18,15 @@ function MyStack() {
             <Stack.Screen name="Register" component={Register} options={noHead} />
             <Stack.Screen name="Tabs" component={Tabs} options={noHead} />
             <Stack.Screen name='Discount' component={Discount}/>
+            <Stack.Screen name='Address' component={Address}/>
+            <Stack.Screen name='Checkout' component={Checkout} options={({route}) => ({
+              title: route.params?.title,
+            })}
+            initialParams={{
+              totalPrice: "",
+            }}
+            />
+            <Stack.Screen name='ItemDetail' component={ItemDetail}/>
         </Stack.Navigator>
     </NavigationContainer>
   );
